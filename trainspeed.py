aba686c5293fe1edd435f6003c0597be7c301e6c
paul <bertram.paul@icloud.com>
2023-05-30 20:11:08 +0200

    checks if value didnt change and higher intervall

--- trainspeed.py
+++ trainspeed.py
@@ -7,15 +7,15 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class SpeedIndicatorApp(rumps.App):
     def __init__(self):
-        super(SpeedIndicatorApp, self).__init__("...kmh")
+        super(SpeedIndicatorApp, self).__init__("...km/h")
         self.speed = "Loading..."
         self.error = ""
         self.menu = [self.speed]
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")  # use headless browser
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("https://iceportal.de/")
-        self.timer = rumps.Timer(self.update_speed, 5)
+        self.timer = rumps.Timer(self.update_speed, 0.5)  # update speed more often
         self.timer.start()
 
     def update_speed(self, sender):
@@ -24,8 +24,11 @@ class SpeedIndicatorApp(rumps.App):
                 EC.presence_of_element_located((By.CLASS_NAME, "traininfoIntro-speedIndicatorText"))
             )
             speed = speed_element.text.strip()
-            self.speed = speed
-            self.error = ""
+            if speed != self.speed:  # only update menu if speed has changed
+                self.speed = speed
+                self.error = ""
+                self.menu.clear()
+                self.menu.add(self.speed)
         except Exception as e:
             self.speed = ""
             self.error = f"Error: {str(e)}"
